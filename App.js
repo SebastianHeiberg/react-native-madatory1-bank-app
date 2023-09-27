c94f2997bb92836a91336bdf276c2145f5f09e20
SebastianHeiberg <seba3673@stud.kea.dk>
2023-09-27 15:46:59 +0200

    lavet en stylet new account side

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList, } from 'react-native';
-import { useState } from 'react';
-import AccountItem from './components/AccountItem.js';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View, FlatList, } from 'react-native';
+// import { useState } from 'react';
+// import AccountItem from './components/AccountItem.js';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import NewAccount from './components/NewAccount.js';
+import { Homepage } from './components/Homepage.js';
+
+
+
 export default function App() {
 
-  
-  const Stack = createNativeStackNavigator()
+
+const Stack = createNativeStackNavigator()
 
   return (
     <NavigationContainer>
@@ -19,83 +23,12 @@ export default function App() {
         />
         <Stack.Screen
         name='NewAccount'
-        component={NewAccount}/>
+        component={NewAccount}
+        options={{ title: 'Home' }}/>
       </Stack.Navigator>
     </NavigationContainer>
 
   )
 }
 
-const Homepage = ({navigation, route}) => {
 
-  const [accounts, setAccounts] = useState([{ text: "Budget"}, { text: "Fælles"}, { text: "Opsparing"}])
-
-   return (
-    <View style={styles.container}>
-      <View style={styles.topBox}>
-        <Text>Sebastians bank app</Text>
-      </View>
-      <View style={styles.accountsBox}>
-      <FlatList
-          style={styles.list}
-          data={accounts}
-          renderItem={(itemData) => {
-            return (
-              <AccountItem
-                text={itemData.item.text}
-              />
-            );
-          }}
-        />
-      </View>
-      <View style={styles.createAccount}>
-        <Text onPress={() => navigation.navigate('NewAccount')}>opret ny konto</Text>
-      </View>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'lightblue',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-   topBox: {
-    flex: 1,
-    borderBottomWidth: 5,
-    borderColor: 'black',
-    margin: 10,
-    padding: 5,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '80%',
-    backgroundColor: 'white'
-   },
-   accountsBox: {
-    flex: 6,
-    backgroundColor: 'white',
-    alignItems: 'start',
-    width: '80%',
-    padding: 10,
-    margin: 5,
-    borderWidth: 1,
-    borderColor: 'black'
-   },
-   list: {
-    width: '100%'
-   },
-   createAccount: {
-    flex: 1,
-    borderTopWidth: 5,
-    borderColor: 'black',
-    margin: 10,
-    padding: 5,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '80%',
-    backgroundColor: 'white',
-   }   
-});
--- components/NewAccount.js
+++ components/NewAccount.js
@@ -1,13 +1,82 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text, View, TextInput, StyleSheet} from 'react-native';
 
 const NewAccount = () => {
   return (
-    <View>
-      <Text>This is the account creation page</Text>
-      {/* Add your HTML content here */}
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <View style={styles.inputFields}>
+          <Text style={styles.field}>Navn på kontoen</Text>
+          <TextInput style={styles.inputText}  placeholder='Fx. børneopsparing'/>
+          <Text style={styles.field}>Saldo på kontoen</Text>
+          <TextInput style={styles.inputText} placeholder='Antal i kr.'/>
+          <View style={styles.submitView}>
+          <Text style={styles.submitText}> Opret konto </Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
 
 export default NewAccount;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'lightblue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputContainer: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'start',
+    width: '80%',
+    margin: 5,
+    borderWidth: 1,
+    borderColor: 'black',
+    alignItems: 'center',
+
+
+  },
+  inputFields :{
+    flex:1,
+    flexDirection: 'column',
+    alignItems: 'start',
+    justifyContent: 'start',
+    borderBlockColor: "black",
+    width: '100%'
+  },
+  field: {
+    padding: 5,
+    margin: 10,
+    borderBottomWidth: 3,
+    borderColor: 'black',
+    backgroundColor: 'lightblue',
+    width: '90%'
+
+  },
+  inputText: {
+    margin: 10,
+    padding: 4,
+    width:'90%',
+    borderWidth: 1,
+    borderBlockColor: 'black'
+  },
+  submitText : {
+    borderWidth: 1,
+    borderBlockColor: 'black',
+    backgroundColor: 'lightblue',
+    fontSize: '16px',
+    fontFamily: 'bold'
+  },
+  submitView: {
+    flex:1,
+    justifyContent: 'start',
+    width: '90%',
+    margin: 10,
+    alignItems: 'center'    
+  }
+
+})
